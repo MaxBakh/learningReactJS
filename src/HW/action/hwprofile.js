944d726560e01655415ea95c69a004c30623fdc4
MaxBakh <78961199+MaxBakh@users.noreply.github.com>
2021-09-29 09:43:14 +0300

    lesson 7

---
+++ src/HW/action/hwprofile.js
@@ -0,0 +1,7 @@
+export const CHANGE_NAME = "PROFILE::CHANGE_NAME"
+
+
+export const changeName = (newName) => ({
+    type: CHANGE_NAME,
+    payload: newName,
+});
---
+++ src/store.js
@@ -0,0 +1,19 @@
+import { applyMiddleware, combineReducers, createStore, compose } from 'redux'
+import thunk from 'redux-thunk'
+import profileReducer from './reducers/profile'
+
+const rootReducer = combineReducers({
+    profile: profileReducer,
+})
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ || compose
+
+
+
+
+
+export const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
+)
